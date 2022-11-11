3eb59da8c9149dcdf9a3e69e55f45578d85d138e
mdsds-elt <mss@cesar.org.br>
2022-11-11 13:02:06 -0400

    Add Code

--- sensor_presenca/sensor_presenca.ino
+++ sensor_presenca/sensor_presenca.ino
@@ -11,9 +11,9 @@ bool pir_presence_flag = false;
 void msgTelegramBot(void);
 
 String ssid  = "ICOMP_1A";
-String pass  = "1comp@14#";
+String pass  = "*******";
 
-//Substituir pelo Token do Bot criado.
+
 String token = "5774484856:AAEwZxVarSmfIEqjUlYHd3GdOBbJ2xAVqns";
 
 void IRAM_ATTR ISR_0() {
@@ -21,7 +21,6 @@ void IRAM_ATTR ISR_0() {
 }
 //SetUp
 void setup() {
-  //Pino de interrupção incializado em Pull Down
   pinMode(PIR, INPUT_PULLDOWN);
   attachInterrupt(PIR, ISR_0, RISING);
   Serial.begin(115200);
@@ -61,29 +60,24 @@ bool reconnectTelgramBot() {
 void msgTelegramBot() {
   TBMessage msg;
   while (1) {
-    //Verifica se chegou alguma mensagem - Uma primeira mensagem é necessária ser enviada para iniciar o Bot
+
     if (CTBotMessageText == myBot.getNewMessage(msg)) {
-      //Envia mensagem de volta ao Bot
+
       myBot.sendMessage(msg.sender.id, msg.text);
-      
-      //Quando a primeira mensagem é recebida o MCU começa a monitorar o sensor de presença para enviar mensagens ao Bot
+    
       while (1) {
         //Movimento detectado
         if (pir_presence_flag) {
-          //Sinaliza que ocorreu a interrupção
-          Serial.println("Interrupcao");
+          Serial.println("Interrupcao");//Sinaliza que ocorreu a interrupção
           //Reseta o valor da Flag
           pir_presence_flag = false;
-          //Envia mensagem ao Bot
           if (!myBot.sendMessage(msg.sender.id, "Presença Detectada!")) {
             //Falha no envio da mensagem
             Serial.println("Falha no envio da mensagem.");
-            //Verifica a conexão com o Bot
             while (!reconnectTelgramBot()) {
               delay(10);
             }
-          }
-          //Aguarda 10 segundos para verificar novamente se a flag mudou de valor.
+          
           delay(10000);
         }
         delay(10);
